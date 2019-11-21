606f908a7c81f6ccb4822e141b4f9d418f907711
Ivan002 <ivan.vita92@gmail.com>
2019-11-21 15:02:36 +0100

    update user logout

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLayoutComponent } from '../layout/user-layout/user-layout.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
-  { path: 'user-dashboard', children: [
+  { path: 'user-dashboard', component: UserLayoutComponent, children: [
       { path: '', component: UserDashboardComponent }
   ]}
 ];
