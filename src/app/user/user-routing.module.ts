5049279d1cf46e38d2be3988b6e4626f145fa4a2
Ivan002 <ivan.vita92@gmail.com>
2019-11-21 12:08:42 +0100

    update user

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+
+const routes: Routes = [
+  { path: 'user-dashboard', children: [
+      { path: '', component: UserDashboardComponent }
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
