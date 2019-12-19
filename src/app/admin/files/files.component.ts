3ec0dcaa483b13ae9ab007fa0b3f95995409eda0
Ivan002 <ivan.vita92@gmail.com>
2019-12-19 23:15:43 +0100

    add File crud

--- src/app/admin/files/files.component.ts
+++ src/app/admin/files/files.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FileService } from 'src/service/file.service';
 import { FileDTO } from 'src/dto/filedto';
+import { UserDTO } from 'src/dto/userdto';
+import { TopicDTO } from 'src/dto/topicdto';
+import { TopicService } from 'src/service/topic.service';
 
 @Component({
   selector: 'app-files',
@@ -9,17 +12,26 @@ import { FileDTO } from 'src/dto/filedto';
 })
 export class FilesComponent implements OnInit {
 
+  user : UserDTO;
+  topics : TopicDTO[];
   files: FileDTO[];
   filetoinsert: FileDTO = new FileDTO();
 
-  constructor(private service: FileService) { }
+  constructor(private service: FileService, private tservice : TopicService) { }
 
   ngOnInit() {
+    this.filetoinsert.topicDTO = new TopicDTO();
     this.getFiles();
+    this.getTopics();
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
   }
 
   getFiles() {
     this.service.getAll().subscribe(files => this.files = files);
+  }
+
+  getTopics() {
+    this.tservice.getAll().subscribe(topics => this.topics = topics);
   }
   
   delete(file: FileDTO) {
@@ -31,6 +43,7 @@ export class FilesComponent implements OnInit {
   }
 
   insert(file: FileDTO) {
+    file.userDTO = this.user;
     this.service.insert(file).subscribe(() => this.getFiles());
   }
 
